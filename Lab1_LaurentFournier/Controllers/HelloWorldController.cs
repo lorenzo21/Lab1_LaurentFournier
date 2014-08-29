d2918ef7d830fb1743682d3f112516d91d0813aa
lorenzo21 <lauwarrior@yahoo.ca>
2014-08-29 15:03:59 -0400

    Vue créée

--- Lab1_LaurentFournier/Controllers/HelloWorldController.cs
+++ Lab1_LaurentFournier/Controllers/HelloWorldController.cs
@@ -11,17 +11,20 @@ namespace Lab1_LaurentFournier.Controllers
         // 
         // GET: /HelloWorld/ 
 
-        public string Index()
+        public ActionResult Index()
         {
-            return "This is my <b>default</b> action...";
+            return View();
         }
 
         // 
         // GET: /HelloWorld/Welcome/ 
 
-        public string Welcome(string name, int numTimes = 1)
+        public ActionResult Welcome(string name, int numTimes = 1)
         {
-            return HttpUtility.HtmlEncode("Hello " + name + ", NumTimes is: " + numTimes);
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
         }
     }
 }
